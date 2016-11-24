a3a439f7dce7f29c18dbe4f93a17ea469e7a3b7e
Kyle Air <cpkmanchee@gmail.com>
2016-11-23 21:16:44 -0500

    reorganized subfolders of monitor

---
+++ Regen monitor/logging/monitor_log_parse.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 09:32:19 2016
+
+@author: cpkmanchee
+"""
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+filedir = '/Users/cpkmanchee/Google Drive/PhD/Data/2016-11-22 DILAS temp profile'
+fileMON = '2016-11-22 MON temperature profile 10W.txt'
+filePOW = '2016-11-22 PM100 temperature profile 10W.txt'
+
