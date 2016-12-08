11507940ac6ce9023c277116b4eaf1753db54987
Kyle Air <cpkmanchee@gmail.com>
2016-12-08 09:54:46 -0500

    created to simulate diffraction grating pair. Use to calculate various parameters

---
+++ diffraction_grating_pair.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 19 11:02:23 2016
+
+@author: cpkmanchee
+
+Notes:
+
+Diffraction grating pair
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+Simulate grating pair
+pulse = input pulse object
+L = grating separation (m), use (-) L for stretcher, (+) L for compressor geometry
+N = lns/mm of gratings
+AOI = angle of incidence (deg)
+
+theta = diffraction angle (assumed 1 order, as is standard)
+d = groove spacing
+
+EVERYTHING IS FOR 1st order
+'''
+
+#constants
+h = 6.62606957E-34  #J*s
+c = 299792458.0     #m/s
+
+def gdd2len(GDD, N, AOI, lambda0):
+    m = 1
+    g = AOI*np.pi/180    #convert AOI into rad
+    d = 1E-3/N    #gives grove spacing in m
+
+    w0 = 2*np.pi*c/lambda0
+    theta = np.arcsin(m*2*np.pi*c/(w0*d) - np.sin(g))
+
+    L = np.abs(GDD*(d**2*w0**3*np.cos(theta)**3)/(-m**2*2*4*(np.pi**2)*c))
+
+    L_real = L/np.cos(theta)    
+    
+    return L, L_real
+    
+def beta2(N, AOI, lambda0):
+    m = 1
+    g = AOI*np.pi/180    #convert AOI into rad
+    d = 1E-3/N    #gives grove spacing in m
+
+    w0 = 2*np.pi*c/lambda0
+    theta = np.arcsin(m*2*np.pi*c/(w0*d) - np.sin(g))
+    
+    beta2 = (-m**2*2*4*(np.pi**2)*c)/(d**2*w0**3*np.cos(theta)**3)
+
+    return beta2
+    
+
+def dispCoef(L, N, AOI, lambda0):
+    m = 1
+    g = AOI*np.pi/180    #convert AOI into rad
+    d = 1E-3/N    #gives grove spacing in m
+
+    w0 = 2*np.pi*c/lambda0
+    theta = np.arcsin(m*2*np.pi*c/(w0*d) - np.sin(g))
+    
+    phi0 = 4*L*w0*np.cos(theta)/c
+    phi1 = (phi0/w0)*(1+(2*np.pi*c*m*np.sin(theta)/(w0*d*np.cos(theta)**2)))
+    phi2 = (-m**2*2*4*(np.pi**2)*L*c/(d**2*w0**3))*(1/np.cos(theta)**3)
+    phi3 = (-3*phi2/w0)*(1+(2*np.pi*c*m*np.sin(theta)/(w0*d*np.cos(theta)**2)))
+    phi4 = ((2*phi3)**2/(3*phi2)) + phi2*(2*np.pi*c*m/(w0**2*d*np.cos(theta)**2))**2
+    
+    return np.array([phi0,phi1,phi2,phi3,phi4])
+    
+
+def diffAngle(N, AOI, lambda0):
+    m = 1
+    g = AOI*np.pi/180    #convert AOI into rad
+    d = 1E-3/N    #gives grove spacing in m
+
+    w0 = 2*np.pi*c/lambda0
+    theta = np.arcsin(m*2*np.pi*c/(w0*d) - np.sin(g))
+    
+    return theta*180/np.pi
+    
+def transBeamSize(GDD, N, AOI, lambda0, dlambda):
+    
+    L, L_real = gdd2len(GDD, N, AOI, lambda0)
+    dth = np.abs(diffAngle(N, AOI, lambda0 + dlambda/2) - diffAngle(N, AOI, lambda0-dlambda/2))
+    
+    dxMax = 2*L_real*np.arctan(dth*np.pi/(2*180))
+    
+    return dxMax
+    
+    
